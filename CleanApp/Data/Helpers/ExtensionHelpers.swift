218850888722c0823ef8dc9ae78d7c68d5e85ecd
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-03-16 14:57:05 -0300

    Add Data extension for JSON convertion

--- CleanApp/Data/Helpers/ExtensionHelpers.swift
+++ CleanApp/Data/Helpers/ExtensionHelpers.swift
@@ -4,4 +4,11 @@ public extension Data {
     func toModel<T: Decodable>() -> T? {
         try? JSONDecoder().decode(T.self, from: self)
     }
+
+    func toJson() -> [String: Any]? {
+        try? JSONSerialization.jsonObject(
+            with: self,
+            options: .allowFragments
+        ) as? [String: Any]
+    }
 }
