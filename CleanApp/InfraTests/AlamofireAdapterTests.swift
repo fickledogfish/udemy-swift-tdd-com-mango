d3d9e7dfe563b96e3531ffe251b531e016746179
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-03-16 14:08:04 -0300

    Ensure AlamofireAdapter calls Alamofire with correct URL

--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -1,7 +1,64 @@
 import XCTest
+import Alamofire
+
+class AlamofireAdapter {
+    private let session: Session
+
+    init(session: Session = .default) {
+        self.session = session
+    }
+
+    func post(to url: URL) {
+        session
+            .request(url)
+            .resume()
+    }
+}
 
 class AlamofireAdapterTests: XCTestCase {
     func test_() {
+        // Arrange
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
 
+        let exp = expectation(description: "waiting")
+        
+        let sut = AlamofireAdapter(session: session)
+
+        // Act
+        sut.post(to: url)
+
+        // Assert
+        UrlProtocolStub.observeRequest { request in
+            XCTAssertEqual(url, request.url)
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+class UrlProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
+    }
+
+    open class override func canInit(with request: URLRequest) -> Bool {
+        true
+    }
+
+    open class override func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+
+    override func startLoading() {
+        UrlProtocolStub.emit?(request)
+    }
+
+    override func stopLoading() {
     }
 }
