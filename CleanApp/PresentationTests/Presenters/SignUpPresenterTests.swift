dc6b9dd78bc6d6969eee22780c66dca58bcbaeb1
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-03-21 13:57:39 -0300

    Organize Presentation classes

--- CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
+++ CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -1,63 +1,6 @@
 import XCTest
 
-class SignUpPresenter {
-    private let alertView: AlertView
-
-    init(alertView: AlertView) {
-        self.alertView = alertView
-    }
-
-    func signUp(viewModel: SignUpViewModel) {
-        if let message = validate(viewModel: viewModel) {
-            alertView.showMessage(viewModel: AlertViewModel(
-                title: "Falha na validação",
-                message: message
-            ))
-        }
-    }
-
-    private func validate(viewModel: SignUpViewModel) -> String? {
-        if viewModel.name == nil || viewModel.name!.isEmpty {
-            return "O campo nome é obrigatório"
-        } else if viewModel.email == nil || viewModel.email!.isEmpty {
-            return "O campo email é obrigatório"
-        } else if viewModel.password == nil || viewModel.password!.isEmpty {
-            return  "O campo senha é obrigatório"
-        } else if viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty {
-            return "O campo confirmação de senha é obrigatório"
-        }
-
-        return nil
-    }
-}
-
-struct SignUpViewModel {
-    var name: String?
-    var email: String?
-    var password: String?
-    var passwordConfirmation: String?
-
-    public init(
-        name: String?,
-        email: String?,
-        password: String?,
-        passwordConfirmation: String?
-    ) {
-        self.name = name
-        self.email = email
-        self.password = password
-        self.passwordConfirmation = passwordConfirmation
-    }
-}
-
-struct AlertViewModel: Equatable {
-    var title: String
-    var message: String
-}
-
-protocol AlertView {
-    func showMessage(viewModel: AlertViewModel)
-}
+import Presentation
 
 class SignUpPresenterTests: XCTestCase {
     func test_signUp_should_show_error_message_if_name_is_not_provided() {
