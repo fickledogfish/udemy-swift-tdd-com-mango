ad1064d38b1c8e3e2e2aa6e18bc78d47a8a05810
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-04-12 10:07:09 -0300

    Move SignUpViewModel to its own file

--- CleanApp/Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ CleanApp/Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -76,22 +76,3 @@ public final class SignUpPresenter {
         return nil
     }
 }
-
-public struct SignUpViewModel {
-    public var name: String?
-    public var email: String?
-    public var password: String?
-    public var passwordConfirmation: String?
-
-    public init(
-        name: String?,
-        email: String?,
-        password: String?,
-        passwordConfirmation: String?
-    ) {
-        self.name = name
-        self.email = email
-        self.password = password
-        self.passwordConfirmation = passwordConfirmation
-    }
-}
---
+++ CleanApp/Presentation/Presenters/SignUp/SignUpViewModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct SignUpViewModel {
+    public var name: String?
+    public var email: String?
+    public var password: String?
+    public var passwordConfirmation: String?
+
+    public init(
+        name: String?,
+        email: String?,
+        password: String?,
+        passwordConfirmation: String?
+    ) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
+}
