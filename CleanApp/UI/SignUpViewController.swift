402652aee5c3150982da205f00faf2de122ed8bd
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-04-14 15:37:31 -0300

    Implement LoadingView protocol for SignUpViewController

--- CleanApp/UI/SignUpViewController.swift
+++ CleanApp/UI/SignUpViewController.swift
@@ -1,9 +1,21 @@
 import UIKit
 
+import Presentation
+
 final class SignUpViewController: UIViewController {
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension SignUpViewController: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        if viewModel.isLoading {
+            loadingIndicator.startAnimating()
+        } else {
+            loadingIndicator.stopAnimating()
+        }
     }
 }
