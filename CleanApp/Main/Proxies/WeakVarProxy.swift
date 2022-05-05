fdc1b2d4ca20224411e6f4de3c9a7fe792041d62
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-05-05 10:13:02 -0300

    Move WeakVarProxy to its own file

---
+++ CleanApp/Main/Proxies/WeakVarProxy.swift
@@ -0,0 +1,21 @@
+import Presentation
+
+final class WeakVarProxy<T: AnyObject> {
+    private weak var instance: T?
+
+    init(_ instance: T) {
+        self.instance = instance
+    }
+}
+
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
+}
