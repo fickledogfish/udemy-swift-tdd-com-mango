e4d7f25e41cc46e8296e0f836f9e9ef9539af230
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-05-05 17:20:39 -0300

    Move SUT creation to a factory method

--- CleanApp/MainTests/Composers/SignUpComposerTests.swift
+++ CleanApp/MainTests/Composers/SignUpComposerTests.swift
@@ -2,13 +2,28 @@ import XCTest
 
 import Main
 import PresentationTests
+import UI
 
 class SignUpComposerTests: XCTestCase {
-    func test_ui_presentation_integration() {
+    func test_background_request_should_complete_on_main_thread() {
+        let (sut, _) = makeSut()
+        sut.loadViewIfNeeded()
+    }
+}
+
+extension SignUpComposerTests {
+    func makeSut(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy) {
+        let addAccountSpy = AddAccountSpy()
         let sut = SignUpComposer.composeControllerWith(
-            addAccount: AddAccountSpy()
+            addAccount: addAccountSpy
         )
 
-        checkMemoryLeak(for: sut)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: addAccountSpy, file: file, line: line)
+
+        return (sut, addAccountSpy)
     }
 }
