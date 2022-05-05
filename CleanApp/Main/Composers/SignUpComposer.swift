c63aa2840e2b1be6c6ed979458ba597de6df39cd
Aníbal Vilela <anibal.vilela@vaivoa.com>
2022-05-05 16:38:23 -0300

    Remove controller factory

--- CleanApp/Main/Composers/SignUpComposer.swift
+++ CleanApp/Main/Composers/SignUpComposer.swift
@@ -1,10 +1,24 @@
-import Domain
 import UI
+import Presentation
+import Validation
+import Domain
 
 public final class SignUpComposer {
     public static func composeControllerWith(
         addAccount: AddAccount
     ) -> SignUpViewController {
-        ControllerFactory.makeSignUp(addAccount: addAccount)
+        let controller = SignUpViewController.instantiate()
+
+        let emailValidatorAdapter = EmailValidatorAdapter()
+
+        let presenter = SignUpPresenter(
+            alertView: WeakVarProxy(controller),
+            emailValidator: emailValidatorAdapter,
+            addAccount: addAccount,
+            loadingView: WeakVarProxy(controller)
+        )
+        controller.signUp = presenter.signUp
+
+        return controller
     }
 }
