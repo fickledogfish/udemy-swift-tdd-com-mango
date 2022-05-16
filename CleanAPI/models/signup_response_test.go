97fe7ce4f582ebaf4c986127d35d3a490f7a190f
Aníbal Vilela <88333972+fickledogfish@users.noreply.github.com>
2022-05-16 15:36:59 -0300

    SignUpResponse tests

--- CleanAPI/models/signup_response_test.go
+++ CleanAPI/models/signup_response_test.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +10,47 @@ import (
 
 func TestEnsureSignUpResponseImplementsBinaryMarshaler(t *testing.T) {
 	assert.Implements(t, (*encoding.BinaryMarshaler)(nil), new(signUpResponse))
+}
+
+func TestNewSignUpResponseShouldInitializeAllFields(t *testing.T) {
+	// Arrange
+	name := "some_name"
+	accessToken := "some_access_token"
+
+	user := User{
+		Name:        name,
+		AccessToken: accessToken,
+	}
+
+	// Act
+	sut := NewSignUpResponse(user)
+
+	// Assert
+	assert.Equal(t, name, sut.Name)
+	assert.Equal(t, accessToken, sut.AccessToken)
+}
+
+func TestMarshalBinaryShouldJSONifyTheStruct(t *testing.T) {
+	// Arrange
+	name := "some_name"
+	accessToken := "some_token"
+
+	sut := signUpResponse{
+		Name:        name,
+		AccessToken: accessToken,
+	}
+
+	// Act
+	bytes, err := sut.MarshalBinary()
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf(
+		"{"+
+			"\"name\":\"%s\","+
+			"\"accessToken\":\"%s\""+
+			"}",
+		name,
+		accessToken,
+	), string(bytes))
 }
