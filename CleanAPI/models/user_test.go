5ae1ed13227b17f2609b602452671439ee53a1c3
Aníbal Vilela <88333972+fickledogfish@users.noreply.github.com>
2022-05-30 18:44:25 -0300

    Ensure NewUser returns an error if the password hasher returns an error

--- CleanAPI/models/user_test.go
+++ CleanAPI/models/user_test.go
@@ -1,37 +1,80 @@
 package models
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	crypttest "example.com/api/crypt/test"
 )
 
 func TestNewUserShouldHashTheUsersPassword(t *testing.T) {
 	// Arrange
-	name := "some_name"
-	email := "some_email@company.site"
-
-	requestModel := SignUp{
-		Name:  name,
-		Email: email,
-	}
-
 	expectedPasswordHash := "some_hash"
 
-	passwordHasher := crypttest.NewPasswordHasherMock(
-		func(string) ([]byte, error) {
-			return []byte(expectedPasswordHash), nil
-		},
-	)
+	sut := makeUserSut()
+	sut.PasswordHasher.CompleteWith = func(string) ([]byte, error) {
+		return []byte(expectedPasswordHash), nil
+	}
 
 	// Act
-	user, err := NewUser(passwordHasher, requestModel)
+	user, err := sut.NewUser()
 
 	// Assert
 	assert.NoError(t, err)
-	assert.Equal(t, name, user.Name)
-	assert.Equal(t, email, user.Email)
+	assert.Equal(t, sut.Model.Name, user.Name)
+	assert.Equal(t, sut.Model.Email, user.Email)
 	assert.Equal(t, string(user.PasswordHash), expectedPasswordHash)
+}
+
+func TestNewUserShouldReturnErrorIfThePasswordHasherReturnsAnError(t *testing.T) {
+	// Arrange
+	expectedError := errors.New("C++")
+
+	sut := makeUserSut()
+	sut.PasswordHasher.CompleteWith = func(string) ([]byte, error) {
+		return []byte{}, expectedError
+	}
+
+	// Act
+	_, err := sut.NewUser()
+	require.Error(t, err)
+
+	// Assert
+	assert.ErrorIs(t, err, expectedError)
+}
+
+// File SUT -------------------------------------------------------------------
+
+type userSut struct {
+	PasswordHasher *crypttest.PasswordHasherMock
+
+	Model *SignUp
+}
+
+func makeUserSut() userSut {
+	passwordHasher := crypttest.NewPasswordHasherMock(
+		func(string) ([]byte, error) {
+			return []byte{}, nil
+		},
+	)
+
+	signUpModel := SignUp{
+		Name:                 "some_name",
+		Email:                "some_email",
+		Password:             "some_password",
+		PasswordConfirmation: "some_password_confirmation",
+	}
+
+	return userSut{
+		PasswordHasher: &passwordHasher,
+
+		Model: &signUpModel,
+	}
+}
+
+func (sut *userSut) NewUser() (User, error) {
+	return NewUser(sut.PasswordHasher, *sut.Model)
 }
