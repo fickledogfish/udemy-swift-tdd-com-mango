9c63686be64d1ebaac0e6141edcbf64feeec725e
Aníbal Vilela <88333972+fickledogfish@users.noreply.github.com>
2022-09-20 18:59:38 -0300

    Move responses tests to a suite

--- CleanAPI/router/responses/responses_test.go
+++ CleanAPI/router/responses/responses_test.go
@@ -9,152 +9,139 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestWriteResponseShouldAnswerWithInternalErrorOnMarshalFailure(t *testing.T) {
-	// Arrange
-	sut := makeResponsesSut()
-	sut.Response.MarshalWith = func() ([]byte, error) {
-		return []byte{}, errors.New("error")
-	}
-
-	// Act
-	writeResponse(sut.ResponseRecorder, http.StatusOK, sut.Response)
-
-	// Assert
-	assert.Equal(
-		t,
-		"{\"error\":\"Internal server error\"}",
-		sut.ReadResponseBody(t),
-	)
+func TestResponsesSuite(t *testing.T) {
+	suite.Run(t, new(responsesSuite))
 }
 
-func TestEnsureOkWritesStatusHeaderAndResponse(t *testing.T) {
-	// Arrange
-	message := "ok"
+type responsesSuite struct {
+	suite.Suite
 
-	sut := makeResponsesSut()
-	sut.Response.Message = message
-
-	// Act
-	Ok(sut.ResponseRecorder, sut.Response)
-
-	// Assert
-	assert.Equal(t, http.StatusOK, sut.ResponseRecorder.Code)
-	assert.Equal(
-		t,
-		fmt.Sprintf("{\"message\":\"%s\"}", message),
-		sut.ReadResponseBody(t),
-	)
-}
-
-func TestEnsureBadRequestWritesStatusHeaderAndResponse(t *testing.T) {
-	// Arrange
-	message := "bad request"
-
-	sut := makeResponsesSut()
-	sut.Response.Message = message
-
-	// Act
-	BadRequest(sut.ResponseRecorder, message)
-
-	// Assert
-	assert.Equal(t, http.StatusBadRequest, sut.ResponseRecorder.Code)
-	assert.Equal(
-		t,
-		fmt.Sprintf("{\"error\":\"%s\"}", message),
-		sut.ReadResponseBody(t),
-	)
-}
-
-func TestEnsureInternalServerErrorWritesStatusHeaderAndResponse(t *testing.T) {
-	// Arrange
-	sut := makeResponsesSut()
-
-	// Act
-	InternalServerError(sut.ResponseRecorder)
-
-	// Assert
-	assert.Equal(t, http.StatusInternalServerError, sut.ResponseRecorder.Code)
-	assert.Equal(
-		t,
-		"{\"error\":\"Internal server error\"}",
-		sut.ReadResponseBody(t),
-	)
-}
-
-func TestEnsureForbiddenWritesStatusHeaderAndResponse(t *testing.T) {
-	// Arrange
-	message := "forbidden"
-
-	sut := makeResponsesSut()
-	sut.Response.Message = message
-
-	// Act
-	Forbidden(sut.ResponseRecorder, message)
-
-	// Assert
-	assert.Equal(t, http.StatusForbidden, sut.ResponseRecorder.Code)
-	assert.Equal(
-		t,
-		fmt.Sprintf("{\"error\":\"%s\"}", message),
-		sut.ReadResponseBody(t),
-	)
-}
-
-func TestEnsureNotFoundWritesStatusHeaderAndResponse(t *testing.T) {
-	// Arrange
-	sut := makeResponsesSut()
-
-	// Act
-	NotFound(sut.ResponseRecorder)
-
-	// Assert
-	assert.Equal(t, http.StatusNotFound, sut.ResponseRecorder.Code)
-	assert.Equal(
-		t,
-		"{\"error\":\"Not found\"}",
-		sut.ReadResponseBody(t),
-	)
-}
-
-// Helper types ---------------------------------------------------------------
-
-type DummyResponse struct {
-	Message string `json:"message"`
-
-	MarshalWith func() ([]byte, error) `json:"-"`
-}
-
-func (d DummyResponse) MarshalBinary() ([]byte, error) {
-	if d.MarshalWith != nil {
-		return d.MarshalWith()
-	}
-
-	return json.Marshal(d)
-}
-
-// File SUT -------------------------------------------------------------------
-
-type responsesSut struct {
 	ResponseRecorder *httptest.ResponseRecorder
-	Response         DummyResponse
+	Response         dummyResponse
 }
 
-func makeResponsesSut() responsesSut {
-	return responsesSut{
-		ResponseRecorder: httptest.NewRecorder(),
-		Response: DummyResponse{
-			Message: "some_message",
+func (s *responsesSuite) SetupTest() {
+	s.ResponseRecorder = httptest.NewRecorder()
+
+	s.Response = dummyResponse{
+		Message: "some_message",
+		MarshalWith: func(d dummyResponse) ([]byte, error) {
+			return json.Marshal(d)
 		},
 	}
 }
 
-func (r responsesSut) ReadResponseBody(t *testing.T) string {
-	body, err := ioutil.ReadAll(r.ResponseRecorder.Body)
-	require.NoError(t, err)
+// Test cases -----------------------------------------------------------------
+
+func (s responsesSuite) TestWriteResponseShouldAnswerWithInternalErrorOnMarshalFailure() {
+	// Arrange
+	s.Response.MarshalWith = func(d dummyResponse) ([]byte, error) {
+		return []byte{}, errors.New("error")
+	}
+
+	// Act
+	writeResponse(s.ResponseRecorder, http.StatusOK, s.Response)
+
+	// Assert
+	s.Assert().Equal(
+		"{\"error\":\"Internal server error\"}",
+		s.readResponseBody(),
+	)
+}
+
+func (s responsesSuite) TestEnsureOkWritesStatusHeaderAndResponse() {
+	// Arrange
+	message := "ok"
+
+	s.Response.Message = message
+
+	// Act
+	Ok(s.ResponseRecorder, s.Response)
+
+	// Assert
+	s.Assert().Equal(http.StatusOK, s.ResponseRecorder.Code)
+	s.Assert().Equal(
+		fmt.Sprintf("{\"message\":\"%s\"}", message),
+		s.readResponseBody(),
+	)
+}
+
+func (s responsesSuite) TestEnsureBadRequestWritesStatusHeaderAndResponse() {
+	// Arrange
+	message := "bad request"
+
+	s.Response.Message = message
+
+	// Act
+	BadRequest(s.ResponseRecorder, message)
+
+	// Assert
+	s.Assert().Equal(http.StatusBadRequest, s.ResponseRecorder.Code)
+	s.Assert().Equal(
+		fmt.Sprintf("{\"error\":\"%s\"}", message),
+		s.readResponseBody(),
+	)
+}
+
+func (s responsesSuite) TestEnsureInternalServerErrorWritesStatusHeaderAndResponse() {
+	// Act
+	InternalServerError(s.ResponseRecorder)
+
+	// Assert
+	s.Assert().Equal(http.StatusInternalServerError, s.ResponseRecorder.Code)
+	s.Assert().Equal(
+		"{\"error\":\"Internal server error\"}",
+		s.readResponseBody(),
+	)
+}
+
+func (s responsesSuite) TestEnsureForbiddenWritesStatusHeaderAndResponse() {
+	// Arrange
+	message := "forbidden"
+
+	s.Response.Message = message
+
+	// Act
+	Forbidden(s.ResponseRecorder, message)
+
+	// Assert
+	s.Assert().Equal(http.StatusForbidden, s.ResponseRecorder.Code)
+	s.Assert().Equal(
+		fmt.Sprintf("{\"error\":\"%s\"}", message),
+		s.readResponseBody(),
+	)
+}
+
+func (s responsesSuite) TestEnsureNotFoundWritesStatusHeaderAndResponse() {
+	// Act
+	NotFound(s.ResponseRecorder)
+
+	// Assert
+	s.Assert().Equal(http.StatusNotFound, s.ResponseRecorder.Code)
+	s.Assert().Equal(
+		"{\"error\":\"Not found\"}",
+		s.readResponseBody(),
+	)
+}
+
+// Helpers --------------------------------------------------------------------
+
+func (s responsesSuite) readResponseBody() string {
+	body, err := ioutil.ReadAll(s.ResponseRecorder.Body)
+	s.Require().NoError(err)
 
 	return string(body)
+}
+
+type dummyResponse struct {
+	Message string `json:"message"`
+
+	MarshalWith func(d dummyResponse) ([]byte, error) `json:"-"`
+}
+
+func (d dummyResponse) MarshalBinary() ([]byte, error) {
+	return d.MarshalWith(d)
 }
